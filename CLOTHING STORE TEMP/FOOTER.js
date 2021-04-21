9a20e2d5374ec4af2d724628aa561bc2d70bdba7
MichaelTaveras <78162666+MichaelTaveras@users.noreply.github.com>
2021-04-21 00:46:05 -0400

    Add files via upload

---
+++ CLOTHING STORE TEMP/FOOTER.js
@@ -0,0 +1,16 @@
+//
+// The Internet BookShop - Footer File
+//
+// This file contains the bottom footer for all documents
+// on this website.  We can insert the tag:
+//	<script language="javascript" src="footer.js"></script>
+// to present this menu bar at the bottom of each document
+//
+
+document.write("<CENTER><table><TR height=\"50\"></tr><tr><td width = \"100%\">")
+document.write("  	<p align=\"center\"><font size=\"2\">");
+document.write("        <BR/><strong>Copyright 2021 - Clothing Store LLC </strong><BR/>");
+document.write("		<strong>All rights reserved.</strong><BR/>");
+//document.write("Designated trademarks and brands are the property of their respective owners.</font></p>");
+//document.write("respective owners.</font></p>"); 
+//document.write("</td></tr></table></CENTER>");
